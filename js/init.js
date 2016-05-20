6991e2e1b963dfb66748591e3e3956d68c6b1439
Carlos Velasco <carlos@2activedesign.com>
2016-05-20 16:44:27 +0200

    Insert Sample Content

--- js/init.js
+++ js/init.js
@@ -1,20 +1,4 @@
 (function jQ($) {
-  const options = [{
-    selector: '#sponsors',
-    offset: 500,
-    callback: function Ca() {
-      function nextCarousel() {
-        $('.carousel').carousel('next');
-      }
-      window.setInterval(nextCarousel, 6000);
-    },
-  }, {
-    selector: '#contact',
-    offset: 220,
-    callback: function Sta() {
-      Materialize.showStaggeredList('#contactList');
-    },
-  }];
   $('.button-collapse').sideNav({
     menuWidth: 320,
     // Default is 240
@@ -32,14 +16,6 @@
     interval: 6000,
   });
   $('.modal-trigger').leanModal();
-  $('.carousel').carousel({
-    time_constant: 400,
-    dist: -48,
-    shift: 2,
-    padding: 48,
-    // full_width: 100
-  });
-  Materialize.scrollFire(options);
   $('.hidden').hide();
   $('.grid').masonry({
     // options
