deec1456250a7e53dc7661a35a4054eed14d2fd5
Carlos Velasco <carlos@2activedesign.com>
2016-05-24 12:53:25 +0200

    Multiple Updates

--- js/init.js
+++ js/init.js
@@ -8,21 +8,21 @@
     // Closes side-nav on <a> clicks, useful for Angular/Meteor
   });
   $('.scrollspy').scrollSpy();
-  $('.slider').slider({
-    full_width: true,
-    indicators: true,
-    height: 600,
-    transition: 500,
-    interval: 6000,
-  });
+  // $('.slider').slider({
+  //   full_width: true,
+  //   indicators: true,
+  //   height: 600,
+  //   transition: 500,
+  //   interval: 6000,
+  // });
   $('.modal-trigger').leanModal();
-  $('.hidden').hide();
-  $('.grid').masonry({
-    // options
-    itemSelector: '.grid-item',
-    columnWidth: '.grid-item',
-    percentPosition: true,
-  });
+  // $('.hidden').hide();
+  // $('.grid').masonry({
+  //   // options
+  //   itemSelector: '.grid-item',
+  //   columnWidth: '.grid-item',
+  //   percentPosition: true,
+  // });
   $('.tabs-wrapper').pushpin({
     top: $('.tabs-wrapper').offset().top,
     offset: $('.nav-wrapper').height(),
