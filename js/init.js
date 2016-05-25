b22df3320d5f3c22aaba647364272051951b4b5c
Carlos Velasco <carlos@2activedesign.com>
2016-05-25 16:40:26 +0200

    Color Change to Blue

--- js/init.js
+++ js/init.js
@@ -1,14 +1,14 @@
 (function jQ($) {
   $('.grid').masonry({});
-  $('.button-collapse').sideNav({
-    menuWidth: 320,
-    // Default is 240
-    edge: 'left',
-    // Choose the horizontal origin
-    closeOnClick: true,
-    // Closes side-nav on <a> clicks, useful for Angular/Meteor
-  });
-  $('.scrollspy').scrollSpy();
+  // $('.button-collapse').sideNav({
+  //   menuWidth: 320,
+  //   // Default is 240
+  //   edge: 'left',
+  //   // Choose the horizontal origin
+  //   closeOnClick: true,
+  //   // Closes side-nav on <a> clicks, useful for Angular/Meteor
+  // });
+  // $('.scrollspy').scrollSpy();
   // $('.slider').slider({
   //   full_width: true,
   //   indicators: true,
