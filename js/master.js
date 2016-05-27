3d27e283eaf6562681656c380112e009e006e07e
Carlos Velasco <carlos@2activedesign.com>
2016-05-27 15:38:06 +0200

    Fade Out and In Enabled

--- js/master.js
+++ js/master.js
@@ -15,7 +15,7 @@
   ███████  ██████   ██████ ██   ██    ██    ███████
   */
   function position() {
-    Materialize.toast('Aquiring your location...', 6000);
+    Materialize.toast('Aquiring your location...', 600);
     const options = {
       enableHighAccuracy: true,
       timeout: 60000,
@@ -28,18 +28,18 @@
     }
 
     function error(err) {
-      Materialize.toast('ERROR(' + err.code + '): ' + err.message, 6000, 'rounded');
+      Materialize.toast('ERROR(' + err.code + '): ' + err.message, 1000, 'rounded');
     }
     navigator.geolocation.getCurrentPosition(success, error, options);
   }
 
   function detectLanguage() {
     const lang = window.navigator.language;
-    Materialize.toast('Your preferred language is ' + lang, 6000);
+    Materialize.toast('Your preferred language is ' + lang, 1000);
   }
 
   function mapMsg() {
-    Materialize.toast('Opening Map...', 6000);
+    Materialize.toast('Opening Map...', 600);
   }
   /*
   ███    ██  ██████  ████████ ██ ███████ ██  ██████  █████  ████████ ██  ██████  ███    ██ ███████
@@ -56,29 +56,30 @@
     const n = new Notification(theTitle, options);
   }
 
-  function notifyMe() {
+  function notifyMe(notifyMsg) {
     // Let's check if the browser supports notifications
     if (!('Notification' in window)) {
-      Materialize.toast('This browser does not support desktop notification', 6000);
+      Materialize.toast('This browser does not support desktop notification', 1000);
       // Let's check whether notification permissions have already been granted
     } else if (Notification.permission === 'granted') {
       // If it's okay let's create a notification
-      spawnNotification('New updates!', 'favicon/android-chrome-192x192.png', 'Kullaberg');
+      spawnNotification(notifyMsg, 'favicon/android-chrome-192x192.png', 'Kullaberg');
       // Otherwise, we need to ask the user for permission
     } else if (Notification.permission !== 'denied') {
       Notification.requestPermission(function askFor(permission) {
         // If the user accepts, let's create a notification
         if (permission === 'granted') {
-          spawnNotification('New updates!', 'favicon/android-chrome-192x192.png', 'Kullaberg');
+          spawnNotification(notifyMsg, 'favicon/android-chrome-192x192.png', 'Kullaberg');
+        } else {
+          Notification.requestPermission().then(function giveThe(result) {
+            Materialize.toast('Notification request ' + result, 1000);
+          });
         }
       });
     }
     // At last, if the user has denied notifications, and you
     // want to be respectful there is no need to bother them any more.
   }
-  Notification.requestPermission().then(function giveThe(result) {
-    Materialize.toast('Notification request ' + result, 1000);
-  });
   /*
   ███    ██  ██████  ████████ ███████ ███████
   ████   ██ ██    ██    ██    ██      ██
