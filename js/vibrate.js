43d6fd87e55a5196615fed2f7582360a163e635c
Carlos Velasco <carlos@2activedesign.com>
2016-05-27 22:27:09 +0200

    SW update

--- js/vibrate.js
+++ js/vibrate.js
@@ -10,4 +10,3 @@ self.addEventListener('notificationclick', function swOnClickClose(event) {
   // Close the notification when it is clicked
   event.notification.close();
 });
-Materialize.toast('vibrate.js SW activated', 1000);
