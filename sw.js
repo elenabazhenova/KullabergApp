c9dc9d4cc2db3b7d7e8b7950d40719026a9f30d8
Carlos Velasco <carlos@2activedesign.com>
2016-05-28 11:06:23 +0200

    SW update on notify click

--- sw.js
+++ sw.js
@@ -1,13 +1,14 @@
-self.addEventListener('install', function () {
+var notifyClick = 1;
+self.addEventListener('install', function initInstall() {
   self.skipWaiting();
 });
-
-self.addEventListener('activate', function (event) {
+self.addEventListener('activate', function activeSW(event) {
   event.waitUntil(clients.claim());
 });
-
-// self.addEventListener('notificationclick', function (event) {
-//   // Close the notification when it is clicked
-//
-//   event.notification.close();
-// });
+self.addEventListener('notificationclick', function clickNotify(event) {
+  // Close the notification when it is clicked
+  notifyClick++;
+  if (notifyClick === 3) window.open('', '_self');
+  if (notifyClick === 6) window.open('https://xtreemze.github.com/KullabergApp/', '_self');
+  if (notifyClick === 12) event.notification.close();
+});
