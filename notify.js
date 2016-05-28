32388d9c9e643c52c09d4337e0db4d4d65307bb4
Carlos Velasco <carlos@2activedesign.com>
2016-05-28 09:58:18 +0200

    update notify folder structure

--- notify.js
+++ notify.js
@@ -1,11 +1,11 @@
-navigator.serviceWorker.register('js/sw.min.js');
+navigator.serviceWorker.register('sw.min.js');
 
-function showNotification() {
+function showNotification(notifyMsg) {
   Notification.requestPermission(function (result) {
     if (result === 'granted') {
       navigator.serviceWorker.ready.then(function (registration) {
         registration.showNotification('Kullaberg', {
-          body: 'Buzz! Buzz!',
+          body: notifyMsg,
           icon: 'favicon/android-chrome-192x192.png',
           vibrate: [200, 100, 200, 100, 200, 100, 200],
           tag: 'vibration-sample',
