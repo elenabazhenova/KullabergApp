d54c628e2832b6bdba03110a567da0f811267f37
Carlos Velasco <carlos@2activedesign.com>
2016-06-10 14:54:59 +0200

    contact icon flash

--- js/master.js
+++ js/master.js
@@ -1,12 +1,15 @@
 var notifyAttempts = 0;
 var tagNotify = 'vibrateTest';
-/*
-██       ██████   ██████  █████  ████████ ███████
-██      ██    ██ ██      ██   ██    ██    ██
-██      ██    ██ ██      ███████    ██    █████
-██      ██    ██ ██      ██   ██    ██    ██
-███████  ██████   ██████ ██   ██    ██    ███████
-*/
+const flashContact = function flashContact() {
+  $('.contactIcon').fadeOut(270).fadeIn(270).fadeOut(270).fadeIn(270).fadeOut(270).fadeIn(270);
+};
+  /*
+  ██       ██████   ██████  █████  ████████ ███████
+  ██      ██    ██ ██      ██   ██    ██    ██
+  ██      ██    ██ ██      ███████    ██    █████
+  ██      ██    ██ ██      ██   ██    ██    ██
+  ███████  ██████   ██████ ██   ██    ██    ███████
+  */
 function position() {
   Materialize.toast('Aquiring your location...', 600);
   const options = {
@@ -72,7 +75,7 @@ setInterval(function updateCache() {
 // Check for an updated manifest file every 60 minutes. If it's updated, download a new cache as defined by the new manifest file.
 window.applicationCache.addEventListener('updateready', function downloadedNewCache() { // when an updated cache is downloaded and ready to be used
   window.applicationCache.swapCache();
-// swap to the newest version of the cache
+  // swap to the newest version of the cache
 }, false);
 /*
 ███    ██  ██████  ████████ ███████ ███████
