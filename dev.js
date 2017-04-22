de6f40ce29a17731bb45beed28868e6c403b4ef1
Carlos Velasco <carlos@2activedesign.com>
2017-04-22 16:18:13 +0200

    kmz only offline

--- dev.js
+++ dev.js
@@ -8,7 +8,7 @@ module.exports = function d(env) {
     module: {
       rules: [{
         test: /indexB.html$/,
-        loaders: ['file-loader?name=index.[ext]?[hash]!', 'extract-loader', 'html-loader'],
+        loaders: ['file-loader?name=index.[ext]', 'extract-loader', 'html-loader'],
       }, {
         test: /embedEnB.html$/,
         loaders: ['file-loader?name=embedEn.[ext]', 'extract-loader', 'html-loader'],
@@ -17,35 +17,35 @@ module.exports = function d(env) {
         loaders: ['file-loader?name=embedSv.[ext]', 'extract-loader', 'html-loader'],
       }, {
         test: /mapsB.html$/,
-        loaders: ['file-loader?name=maps.[ext]?[hash]!', 'extract-loader', 'html-loader'],
+        loaders: ['file-loader?name=maps.[ext]', 'extract-loader', 'html-loader'],
       }, {
         test: /newsB.html$/,
-        loaders: ['file-loader?name=news.[ext]?[hash]!', 'extract-loader', 'html-loader'],
+        loaders: ['file-loader?name=news.[ext]', 'extract-loader', 'html-loader'],
       }, {
         test: /poiB.html$/,
-        loaders: ['file-loader?name=poi.[ext]?[hash]!', 'extract-loader', 'html-loader'],
+        loaders: ['file-loader?name=poi.[ext]', 'extract-loader', 'html-loader'],
       }, {
         test: /mapsBSv.html$/,
-        loaders: ['file-loader?name=mapsSv.[ext]?[hash]!', 'extract-loader', 'html-loader'],
+        loaders: ['file-loader?name=mapsSv.[ext]', 'extract-loader', 'html-loader'],
       }, {
         test: /newsBSv.html$/,
-        loaders: ['file-loader?name=newsSv.[ext]?[hash]!', 'extract-loader', 'html-loader'],
+        loaders: ['file-loader?name=newsSv.[ext]', 'extract-loader', 'html-loader'],
       }, {
         test: /poiBSv.html$/,
-        loaders: ['file-loader?name=poiSv.[ext]?[hash]!', 'extract-loader', 'html-loader'],
+        loaders: ['file-loader?name=poiSv.[ext]', 'extract-loader', 'html-loader'],
       }, {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
       }, {
         test: /\.(png|gif|jpg)$/,
-        use: ['file-loader?name=[path][name].[ext]?[hash]!'],
+        use: ['file-loader?name=[path][name].[ext]'],
       }, {
         test: /\.(eot|ttf|woff|woff2)$/,
-        loader: 'file-loader?name=[path][name].[ext]!',
+        loader: 'file-loader?name=[path][name].[ext]',
       }, {
         test: /\.svg$/,
         use: [{
-          loader: 'file-loader?name=[path][name].[ext]?[hash]!',
+          loader: 'file-loader?name=[path][name].[ext]',
         }],
       }, {
         test: /\.js$/,
