2476586ab4a8f643a129f11e32d844dd2f1bb94f
Carlos Velasco <carlos@2activedesign.com>
2017-06-14 17:17:39 +0200

    enable tree shaking

--- dev.js
+++ dev.js
@@ -8,31 +8,49 @@ module.exports = function d(env) {
     module: {
       rules: [{
         test: /indexB.html$/,
-        loaders: ['file-loader?name=index.[ext]', 'extract-loader', 'html-loader'],
+        loaders: ['file-loader?name=index.[ext]', 'extract-loader',
+          'html-loader',
+        ],
       }, {
         test: /embedEnB.html$/,
-        loaders: ['file-loader?name=embedEn.[ext]', 'extract-loader', 'html-loader'],
+        loaders: ['file-loader?name=embedEn.[ext]',
+          'extract-loader', 'html-loader',
+        ],
       }, {
         test: /embedSvB.html$/,
-        loaders: ['file-loader?name=embedSv.[ext]', 'extract-loader', 'html-loader'],
+        loaders: ['file-loader?name=embedSv.[ext]',
+          'extract-loader', 'html-loader',
+        ],
       }, {
         test: /mapsB.html$/,
-        loaders: ['file-loader?name=maps.[ext]', 'extract-loader', 'html-loader'],
+        loaders: ['file-loader?name=maps.[ext]', 'extract-loader',
+          'html-loader',
+        ],
       }, {
         test: /newsB.html$/,
-        loaders: ['file-loader?name=news.[ext]', 'extract-loader', 'html-loader'],
+        loaders: ['file-loader?name=news.[ext]', 'extract-loader',
+          'html-loader',
+        ],
       }, {
         test: /poiB.html$/,
-        loaders: ['file-loader?name=poi.[ext]', 'extract-loader', 'html-loader'],
+        loaders: ['file-loader?name=poi.[ext]', 'extract-loader',
+          'html-loader',
+        ],
       }, {
         test: /mapsBSv.html$/,
-        loaders: ['file-loader?name=mapsSv.[ext]', 'extract-loader', 'html-loader'],
+        loaders: ['file-loader?name=mapsSv.[ext]', 'extract-loader',
+          'html-loader',
+        ],
       }, {
         test: /newsBSv.html$/,
-        loaders: ['file-loader?name=newsSv.[ext]', 'extract-loader', 'html-loader'],
+        loaders: ['file-loader?name=newsSv.[ext]', 'extract-loader',
+          'html-loader',
+        ],
       }, {
         test: /poiBSv.html$/,
-        loaders: ['file-loader?name=poiSv.[ext]', 'extract-loader', 'html-loader'],
+        loaders: ['file-loader?name=poiSv.[ext]', 'extract-loader',
+          'html-loader',
+        ],
       }, {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
@@ -52,7 +70,11 @@ module.exports = function d(env) {
         exclude: [/node_modules/],
         use: [{
           loader: 'babel-loader',
-          options: { presets: ['es2015'] }
+          options: {
+            presets: [
+              ['es2015', { modules: false }],
+            ],
+          },
         }],
       }],
     },
